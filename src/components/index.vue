56f4f8f831637bfd0c90b99dec35596cf3869779
Nine9Dragon <1020854331@qq.com>
2019-10-08 23:40:07 +0800

    index component

---
+++ src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>   
+        <f-button type="primary">进入文档</f-button>
+    </div>
+</template>
+
+<script>
+    import button from './button/src/button'
+    export default {
+        name:'index',
+        components:{
+            'f-button':button
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
