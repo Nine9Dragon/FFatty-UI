00720400dc4c187575ad843f89bce1e619daf319
Nine9Dragon <1020854331@qq.com>
2019-10-13 23:47:57 +0800

    collapse

---
+++ src/components/collapse/collapse.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    export default {
+        name:'f-collapse',
+        data(){
+            return {
+                eventBus:new Vue()
+            }
+        },
+        props:{
+            value:{
+                type:[String,Array],
+                default:()=>{
+                    return []
+                }
+            },
+            accordion: {
+                type: Boolean
+            }
+        },
+        provide () {
+            return { eventBus: this.eventBus }
+        },
+
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
